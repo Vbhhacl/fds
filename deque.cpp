f9ededd508169656b7b73e5c3aab48d0bedbf420
Vbhhacl <139943451+Vbhhacl@users.noreply.github.com>
2024-12-11 19:15:12 +0530

    Update deque.cpp

--- deque.cpp
+++ deque.cpp
@@ -1,111 +1,188 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
 
-class Deque {
-    int arr[10];
-    int front, rear, size;
+using namespace std;
+#define SIZE 5
+
+class dequeue
+{
+
+	int a[10],front,rear,count;
 
 public:
-    Deque() {
-        front = -1;
-        rear = -1;
-        size = 10;
-    }
-
-    bool isFull() {
-        return (front == 0 && rear == size - 1) || (rear + 1 == front);
-    }
-
-    bool isEmpty() {
-        return front == -1;
-    }
-
-    void addFront(int value) {
-        if (isFull()) {
-            cout << "Deque is full!" << endl;
-            return;
-        }
-        if (front == -1) {
-            front = rear = 0;
-        } else if (front == 0) {
-            front = size - 1;
-        } else {
-            front--;
-        }
-        arr[front] = value;
-    }
-
-    void addRear(int value) {
-        if (isFull()) {
-            cout << "Deque is full!" << endl;
-            return;
-        }
-        if (front == -1) {
-            front = rear = 0;
-        } else if (rear == size - 1) {
-            rear = 0;
-        } else {
-            rear++;
-        }
-        arr[rear] = value;
-    }
-
-    void deleteFront() {
-        if (isEmpty()) {
-            cout << "Deque is empty!" << endl;
-            return;
-        }
-        if (front == rear) {
-            front = rear = -1;
-        } else if (front == size - 1) {
-            front = 0;
-        } else {
-            front++;
-        }
-    }
-
-    void deleteRear() {
-        if (isEmpty()) {
-            cout << "Deque is empty!" << endl;
-            return;
-        }
-        if (front == rear) {
-            front = rear = -1;
-        } else if (rear == 0) {
-            rear = size - 1;
-        } else {
-            rear--;
-        }
-    }
-
-    void display() {
-        if (isEmpty()) {
-            cout << "Deque is empty!" << endl;
-            return;
-        }
-        int i = front;
-        while (true) {
-            cout << arr[i] << " ";
-            if (i == rear) break;
-            i = (i + 1) % size;
-        }
-        cout << endl;
-    }
+	dequeue();
+	void add_at_beg(int);
+	void add_at_end(int);
+	void delete_fr_front();
+	void delete_fr_rear();
+	void display();
 };
 
-int main() {
-    Deque dq;
 
-    dq.addRear(10);
-    dq.addRear(20);
-    dq.addFront(5);
-    dq.display();
+dequeue::dequeue()
+{
+	front=-1;
+	rear=-1;
+	count=0;
+}
 
-    dq.deleteFront();
-    dq.display();
 
-    dq.deleteRear();
-    dq.display();
+void dequeue::add_at_beg(int item)
+{
+	int  i;
+	if(front==-1)
+	{
+		front++;
+		rear++;
+		a[rear]=item;
+		count++;
+	}
+	else if(rear>=SIZE-1)
+	{
+		cout<<"\nInsertion is not possible,overflow!!!!";
+	}
+	else
+	{
+		for(i=count;i>=0;i--)
+		{
+			a[i]=a[i-1];
+		}
+		a[i]=item;
+		count++;
+		rear++;
+	}
+}
 
-    return 0;
+
+
+void dequeue::add_at_end(int item)
+{
+
+	if(front==-1)
+	{
+		front++;
+		rear++;
+		a[rear]=item;
+		count++;
+	}
+	else if(rear>=SIZE-1)
+	{
+		cout<<"\nInsertion is not possible,overflow!!!";
+		return;
+	}
+	else
+	{
+		a[++rear]=item;
+	}
+
+
+}
+
+
+
+void dequeue::display()
+{
+
+	for(int i=front;i<=rear;i++)
+	{
+		cout<<a[i]<<" ";	}
+}
+
+
+void dequeue::delete_fr_front()
+{
+	if(front==-1)
+	{
+		cout<<"Deletion is not possible:: Dequeue is empty";
+		return;
+	}
+	else
+	{
+		if(front==rear)
+		{
+			front=rear=-1;
+			return;
+		}
+		cout<<"The deleted element is "<<a[front];
+		front=front+1;
+	}
+
+
+}
+
+void dequeue::delete_fr_rear()
+{
+	if(front==-1)
+	{
+		cout<<"Deletion is not possible:Dequeue is empty";
+		return;
+	}
+	else
+	{
+		if(front==rear)
+		{
+			front=rear=-1;
+		}
+		cout<<"The deleted element is "<< a[rear];
+		rear=rear-1;
+	}
+
+
+}
+
+
+
+int main()
+{
+	int c,item;
+	dequeue d1;
+
+	do
+	{
+		cout<<"\n\n****DEQUEUE OPERATION****\n";
+		cout<<"\n1-Insert at beginning";
+		cout<<"\n2-Insert at end";
+		cout<<"\n3_Display";
+		cout<<"\n4_Deletion from front";
+		cout<<"\n5-Deletion from rear";
+		cout<<"\n6_Exit";
+		cout<<"\nEnter your choice:";
+		cin>>c;
+
+		switch(c)
+		{
+		case 1:
+			cout<<"Enter the element to be inserted:";
+			cin>>item;
+			d1.add_at_beg(item);
+			break;
+
+		case 2:
+			cout<<"Enter the element to be inserted:";
+			cin>>item;
+			d1.add_at_end(item);
+			break;
+
+		case 3:
+			d1.display();
+			break;
+
+		case 4:
+			d1.delete_fr_front();
+			break;
+		case 5:
+			d1.delete_fr_rear();
+			break;
+
+		case 6:
+			exit(1);
+			break;
+
+		default:
+			cout<<"Invalid choice";
+			break;
+		}
+
+	}while(c!=7);
+	return 0;
+
 }
